da7014fe3698a15096d9511f75a17cea329fdd4d
Unknown <berzi.w@gmail.com>
2017-01-24 23:25:58 +0100

    Added static example

---
+++ src/statisk/Main.java
@@ -0,0 +1,12 @@
+package statisk;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Ting();
+		new Ting();
+		new Ting();
+		new Ting();
+	}
+
+}
---
+++ src/statisk/Ting.java
@@ -0,0 +1,15 @@
+package statisk;
+
+public class Ting {
+
+	private static int teller;
+	
+	public Ting() {
+		teller++;
+		System.out.println("Jeg finnes! Antall av meg: " + teller);
+	}
+	
+	
+	
+	
+}
