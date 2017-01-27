82d9aac03441440e45bea70d74f5e46a9c4c3165
nickern <nickern@gmail.com>
2017-01-27 11:10:10 +0100

    Added example without code

---
+++ src/structure/CoolClass_NoCode.java
@@ -0,0 +1,22 @@
+package structure;
+
+public class CoolClass_NoCode {
+	
+	//Global class variables
+		//Static variables
+	
+		//public/private/protected variables
+	
+		//Final variables
+	
+	
+	//Class constructor
+
+	
+	//Methods
+
+	
+	//Getters and setters
+
+}
+
